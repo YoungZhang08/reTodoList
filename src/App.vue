a6dca647942ea01dbee30a3e67ec84f9484f0598
zhangyangyang <895114856@qq.com>
2017-12-05 21:02:54 +0800

    重写vue-todolist

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <h1 v-text="title"></h1>
+    <input v-on:keyup.enter="addList" v-model="newList">
+    <div class="todolist">
+      <ul>
+        <li v-for="item in items" v-bind:class="{finished:item.isFinished}" v-on:click="todoThis(item)">{{item.label}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      title:' Youngzhang want to do!',
+      items:[
+      ],
+      newList:''
+    }
+  },
+  methods:{
+    todoThis(item){
+      item.isFinished = !item.isFinished;
+    },
+    addList(item){
+      this.items.push({
+        label:this.newList,
+        isFinished:false  
+      });
+      this.newList = '';
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.todolist{
+  margin: 0 auto;
+  width: 400px;
+  height: 100%;
+}
+.todolist ul{
+  list-style: none;
+  text-align: left;
+}
+input{
+  width: 350px;
+  height: 20px;
+}
+.finished{
+  text-decoration: line-through;
+}
+</style>
